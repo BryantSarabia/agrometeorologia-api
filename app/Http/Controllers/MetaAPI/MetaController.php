67d057063249d031c7e519446ffd1603cafd43c9
Bryant Sarabia <bryantsarabia@gmail.com>
2020-12-04 20:25:25 +0100

    meta api get and save

--- app/Http/Controllers/MetaAPI/MetaController.php
+++ app/Http/Controllers/MetaAPI/MetaController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\MetaAPI;
 
 use App\Http\Controllers\Controller;
 use App\Models\MetaApiConfiguration;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MetaController extends Controller
 {
+
     public function save(Request $request)
     {
 
@@ -24,32 +27,137 @@ class MetaController extends Controller
             if (!key_exists('sources', $conf['operations'][$key]) || !key_exists('result', $conf['operations'][$key])) {
                 return "missing parameters at " . $key;
             }
+            foreach ($operation['sources'] as $source) {
+                if (!filter_var($source['urlTemplate'], FILTER_VALIDATE_URL)) {
+                    return "invalid source url";
+                }
+
+                if(!is_string($source['description'])){
+                    return "description must be a string";
+                } elseif(!filter_var($source['required'], FILTER_VALIDATE_BOOL)){
+                    return "source required must be a boolean";
+                }
+            }
+
         }
 
         $group = $conf['group'];
         $service = $conf['service'];
         $operations = $conf['operations'];
 
-        $obj = MetaApiConfiguration::where('configuration->group', $group)->where('configuration->service',$service)->first();
-        if($obj){
+        $obj = MetaApiConfiguration::where('configuration->group', $group)->where('configuration->service', $service)->first();
+        if ($obj) {
             return "This conf already exists";
         }
 
-//        $routeFile = fopen(base_path() . '\routes\meta-api.php', "a");
-//        foreach ($operations as $key => $operation) {
-//            $newRoute = "Route::get('/services/" . $group . '/' . $service . '/' . $key . "','MetaController@get');\n";
-//            fwrite($routeFile, $newRoute);
-//        }
-//        fclose($routeFile);
+        $routeFile = fopen(base_path() . '\routes\meta-api.php', "a");
+        foreach ($operations as $key => $operation) {
+            $newRoute = "Route::get('/services/" . $group . '/' . $service . '/' . $key . "','MetaController@get');\n";
+            fwrite($routeFile, $newRoute);
+        }
+        fclose($routeFile);
 
         $obj = MetaApiConfiguration::create([
-           'configuration' => json_encode($conf),
+            'configuration' => json_encode($conf),
         ]);
 
     }
 
-    public function get(Request $request, $group, $service, $operation)
+    public function get(Request $request)
     {
+        $path_array = explode('/', $request->path());
+        $obj = MetaApiConfiguration::where('configuration->group', $path_array[2])->where('configuration->service', $path_array[3])->first();
+        $conf = json_decode($obj->configuration, true);
 
+        // Controllo se la configurazione ha parametri
+        if (key_exists('params', $conf['operations'][$path_array[4]])) {
+            // Controllo se c'è almeno un parametro dentro params
+            if (count($conf['operations'][$path_array[4]]['params']) > 0) {
+                $query_params = $request->query(); // Query params
+                $params = $conf['operations'][$path_array[4]]['params']; // Parametri della configurazione
+                foreach ($params as $key => $param) {
+                    // Controllo se i parametri required sono nella richiesta
+                    if ($param['required']) {
+                        if (!key_exists($key, $query_params)) {
+                            return "Missing param: " . $key;
+                        }
+                        // Setto i parametri non required al valore di default se non sono nella richiesta
+                    } elseif (!$param['required'] && !key_exists($key, $query_params)) {
+                        $query_params[$key] = $param['default'];
+                    }
+                    // Controllo il tipo del parametro
+                    if (!$this->validateType($param['type'], $query_params[$key])) {
+                        return $key . " must be a " . $param['type'];
+                    }
+
+                    // Controllo i limiti
+                    if (!$this->validateLimits($param, $query_params[$key])) {
+                        return $key . " has exceed the limits";
+                    }
+                    $$key = $query_params[$key];
+                }
+            }
+        }
+        // process source
+        $sources = $conf['operations'][$path_array[4]]['sources'];
+        $results = [];
+        foreach ($sources as $key => $source) {
+
+            $url = $source['urlTemplate'];
+            eval("\$url = \"$url\";"); // valuto la url
+            if (filter_var($url, FILTER_VALIDATE_URL)) { // mi assicuro che la url valutata sia sempre una url valida
+                try{
+                    $response = Http::timeout(5)->get($url);
+                    if(!$response->ok() && $source['required']){
+                        return $key . " failed";
+                    }
+                    $results[$key] = $response->json();
+                } catch (ConnectionException $e){
+                    return $key . " failed";
+                }
+
+            }
+        }
+        return response()->json([
+           $results
+        ]);
+    }
+
+    public function validateType($type, $value)
+    {
+        if ($value !== null) {
+            switch ($type) {
+                case 'string':
+                    return is_string($value);
+                    break;
+                case 'integer':
+                    return filter_var($value, FILTER_VALIDATE_INT);
+                    break;
+                case 'float':
+                    return filter_var($value, FILTER_VALIDATE_FLOAT);
+                    break;
+                case 'boolean':
+                    return filter_var($value, FILTER_VALIDATE_BOOL);
+                    break;
+            }
+        }
+    }
+
+    public function validateLimits($param, $value)
+    {
+        if ($value !== null) {
+            if (key_exists('minimum', $param)) {
+                if ($value < $param['minimum']) {
+                    return false;
+                }
+            }
+
+            if (key_exists('maximum', $param)) {
+                if ($value > $param['maximum']) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- app/Traits/UtilityMethods.php
+++ app/Traits/UtilityMethods.php
@@ -4,22 +4,29 @@ namespace App\Traits;
 
 use DateTime;
 
-trait UtilityMethods{
+trait UtilityMethods
+{
 
-    public function validateDate($date, $format = 'Y-m-d'){
+    public function validateDate($date, $format = 'Y-m-d')
+    {
         $d = DateTime::createFromFormat($format, $date);
         return $d && $d->format($format) === $date;
     }
 
-    public function validateID($id){
-        return preg_match('/[^0-9]/',$id);
+    public function validateID($id)
+    {
+        return preg_match('/[^0-9]/', $id);
     }
 
-    public function validateCoordinate($coordinate){
+    public function validateCoordinate($coordinate)
+    {
         return filter_var($coordinate, FILTER_VALIDATE_FLOAT);
     }
 
-    public function validateString($string){
-       return !preg_match('/[^A-Za-z]+/',$string);
+    public function validateString($string)
+    {
+        return !preg_match('/[^A-Za-z]+/', $string);
     }
+
+
 }
